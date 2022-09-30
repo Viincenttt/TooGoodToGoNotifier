09297a27a1a900e8b8c0c457e8284e5f119fb002
vincent kok <vincentkok@live.nl>
2022-09-30 20:49:24 +0200

    Add another unit test to favorites scanner

--- src/scan/test_favoritesscanner.py
+++ src/scan/test_favoritesscanner.py
@@ -10,20 +10,10 @@ class TestFavoritesScanner(unittest.TestCase):
     def test_no_items_available(self):
         # Arrange
         email = 'test@email.com'
-        scanner = FavoritesScanner(email)
+        scanner = FavoritesScanner(email)        
 
-        authenticator_mock = Mock()
-        authenticator_mock.get_access_token.return_value = 'test_access_token'
-        authenticator_mock.user_id = 'test_user_id'
-
-        api_client_mock = Mock()
-        api_client_mock.get_favorites_basket.return_value = [
-            GetFavoritesBasketItemResponse('item-id-1', 'store-1', 0),
-            GetFavoritesBasketItemResponse('item-id-2', 'store-2', 0),
-        ]
-
-        scanner.authenticator = authenticator_mock
-        scanner.client = api_client_mock
+        scanner.authenticator = self.__create_authenticator_mock()
+        scanner.client = self.__create_api_client_mock(0)
 
         # Act
         with self.assertLogs(level='INFO') as log:
@@ -33,10 +23,51 @@ class TestFavoritesScanner(unittest.TestCase):
             self.assertIn('INFO:root:Favorite item status: Store=store-1 Items Available=0', log.output[0])
             self.assertIn('INFO:root:Favorite item status: Store=store-2 Items Available=0', log.output[1])
             self.assertEqual(len(log.output), 2)
-
         
-        authenticator_mock.get_access_token.assert_called_once_with(email)
-        api_client_mock.get_favorites_basket.assert_called_once_with(authenticator_mock.get_access_token.return_value, authenticator_mock.user_id)        
+        scanner.authenticator.get_access_token.assert_called_once_with(email)
+        scanner.client.get_favorites_basket.assert_called_once_with(
+            scanner.authenticator.get_access_token.return_value, 
+            scanner.authenticator.user_id)
+
+    def test_item_available(self):
+        # Arrange
+        email = 'test@email.com'
+        available_items = 1
+        scanner = FavoritesScanner(email)        
+
+        scanner.authenticator = self.__create_authenticator_mock()
+        scanner.client = self.__create_api_client_mock(available_items)
+
+        # Act
+        with self.assertLogs(level='INFO') as log:
+            scanner.scan_favorites()
+
+            # Assert
+            self.assertIn('INFO:root:Favorite item status: Store=store-1 Items Available=0', log.output[0])
+            self.assertIn(f'INFO:root:Favorite item status: Store=store-2 Items Available={available_items}', log.output[1])
+            self.assertIn(f'INFO:root:New favorite item available in Good To Go - Store=store-2 Items Available={available_items}', log.output[2])
+            self.assertEqual(len(log.output), 3)
+        
+        scanner.authenticator.get_access_token.assert_called_once_with(email)
+        scanner.client.get_favorites_basket.assert_called_once_with(
+            scanner.authenticator.get_access_token.return_value, 
+            scanner.authenticator.user_id)
+
+    def __create_authenticator_mock(self) -> Mock:
+        authenticator_mock = Mock()
+        authenticator_mock.get_access_token.return_value = 'test_access_token'
+        authenticator_mock.user_id = 'test_user_id'
+
+        return authenticator_mock
+
+    def __create_api_client_mock(self, available_items: int) -> Mock:
+        api_client_mock = Mock()
+        api_client_mock.get_favorites_basket.return_value = [
+            GetFavoritesBasketItemResponse('item-id-1', 'store-1', 0),
+            GetFavoritesBasketItemResponse('item-id-2', 'store-2', available_items),
+        ]
+
+        return api_client_mock
 
 if __name__ == '__main__':
     unittest.main()
