18403356bcefbd8650d8254140da4cf0eddea1e3
vincent kok <vincentkok@live.nl>
2022-09-30 12:45:38 +0200

    Read authentication token from cache ini file if it exists

--- src/authentication/authenticator.py
+++ src/authentication/authenticator.py
@@ -6,8 +6,15 @@ from authentication.errors import TooGoodToGoLoginPollingError
 import time
 import logging
 from datetime import datetime, timedelta
+import configparser
 
 class Authenticator:
+    CONFIG_FILE_NAME = 'authentication_cache.ini'
+    CONFIG_AUTH_SECTION_NAME = 'TOKENS'
+    CONFIG_AUTH_SECTION_ACCESS_TOKEN = 'AccessToken'
+    CONFIG_AUTH_SECTION_REFRESH_TOKEN = 'RefreshToken'
+    CONFIG_AUTH_SECTION_ACCESS_TOKEN_VALID_UNTIL = 'AccessTokenValidUntil'
+
     def __init__(self, client: ApiClient):
         self.client = client
 
@@ -15,26 +22,36 @@ class Authenticator:
         self.time_between_polling_in_seconds_from = 10
         self.time_between_polling_in_seconds_to = 30
 
-        self.access_token = None
-        self.refresh_token = None
-        self.access_token_valid_until = None   
+        self.config = configparser.ConfigParser()
+        self.config.read(self.CONFIG_FILE_NAME)
+
+        self.access_token = self.config.get(self.CONFIG_AUTH_SECTION_NAME, self.CONFIG_AUTH_SECTION_ACCESS_TOKEN, fallback=None)
+        self.refresh_token = self.config.get(self.CONFIG_AUTH_SECTION_NAME, self.CONFIG_AUTH_SECTION_REFRESH_TOKEN, fallback=None)
+        access_token_valid_until = self.config.get(self.CONFIG_AUTH_SECTION_NAME, self.CONFIG_AUTH_SECTION_ACCESS_TOKEN_VALID_UNTIL, fallback=None)
+        if access_token_valid_until is not None:
+            self.access_token_valid_until = datetime.fromisoformat(access_token_valid_until)
+
+        self.__save_authentication_tokens_to_file('access', 'refresh', datetime.now())
 
     def get_access_token(self, email: str):
         if self.access_token is not None:                        
-            if (self.__should_refresh_access_token):
+            if (self.__should_refresh_access_token()):
                 refresh_token_result = self.client.refresh_access_token(self.refresh_token)
-                self.access_token = refresh_token_result.access_token
-                self.refresh_token = refresh_token_result.refresh_token
-                self.access_token_valid_until = datetime.now() + timedelta(seconds=refresh_token_result.access_token_ttl_seconds)            
+                self.__set_authentication_tokens(
+                    refresh_token_result.access_token, 
+                    refresh_token_result.refresh_token, 
+                    refresh_token_result.access_token_ttl_seconds
+                )
 
             return self.access_token        
 
-        # TODO: Cache access_token in file / settings, so we can load it in app startup
-
         login_result = self.__login(email)
-        self.access_token = login_result.access_token
-        self.refresh_token = login_result.refresh_token
-        self.access_token_valid_until = datetime.now() + timedelta(seconds=login_result.access_token_ttl_seconds)
+        self.__set_authentication_tokens(
+                    login_result.access_token, 
+                    login_result.refresh_token, 
+                    login_result.access_token_ttl_seconds
+        )
+
         return self.access_token
 
     def __login(self, email: str):
@@ -68,3 +85,21 @@ class Authenticator:
         time_to_refresh_access_token = self.access_token_valid_until - timedelta(seconds=600)
 
         return current_time >= time_to_refresh_access_token
+
+    def __set_authentication_tokens(self, access_token: str, refresh_token: str, access_token_ttl_seconds: int):
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        self.access_token_valid_until = datetime.now() + timedelta(seconds=access_token_ttl_seconds)
+
+        self.__save_authentication_tokens_to_file(self.access_token, self.refresh_token, self.access_token_valid_until)
+
+    def __save_authentication_tokens_to_file(self, access_token: str, refresh_token: str, access_token_valid_until: datetime):
+        with open(self.CONFIG_FILE_NAME, 'w') as configfile:
+            if (self.config.has_section(self.CONFIG_AUTH_SECTION_NAME) == False):
+                self.config.add_section(self.CONFIG_AUTH_SECTION_NAME)
+
+            self.config.set(self.CONFIG_AUTH_SECTION_NAME, self.CONFIG_AUTH_SECTION_ACCESS_TOKEN, access_token)
+            self.config.set(self.CONFIG_AUTH_SECTION_NAME, self.CONFIG_AUTH_SECTION_REFRESH_TOKEN, refresh_token)
+            self.config.set(self.CONFIG_AUTH_SECTION_NAME, self.CONFIG_AUTH_SECTION_ACCESS_TOKEN_VALID_UNTIL, access_token_valid_until.isoformat())
+
+            self.config.write(configfile)
