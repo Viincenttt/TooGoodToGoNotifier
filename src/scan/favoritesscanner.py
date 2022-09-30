43f41dd8acec96646ee7b71695b92e02a7647660
vincent kok <vincentkok@live.nl>
2022-09-30 15:56:13 +0200

    Implement logic to scan for favorites with new availability

--- src/scan/favoritesscanner.py
+++ src/scan/favoritesscanner.py
@@ -2,6 +2,7 @@ from api.client import ApiClient
 import logging
 import random
 import time
+from api.models import GetFavoritesBasketItemResponse
 
 from authentication.authenticator import Authenticator
 
@@ -10,6 +11,7 @@ class FavoritesScanner:
         self.email = email
         self.client = client
         self.authenticator = Authenticator(self.client)
+        self.previous_favorites_scan_result = {}
 
         self.time_between_scanning_in_seconds_from = 60
         self.time_between_scanning_in_seconds_to = 300
@@ -28,4 +30,24 @@ class FavoritesScanner:
 
         items = self.client.get_favorites_basket(access_token, user_id)
         for item in items:
-            print(f"Id={item.item_id} Name={item.display_name} Available={item.items_available}")
+            logging.info(f'Favorite item status: Store={item.display_name} Items Available={item.items_available}')
+
+            if self.__is_item_newly_available(item):
+                self.__notify_new_item_available(item)
+
+            self.previous_favorites_scan_result[item.item_id] = item.items_available
+
+    def __is_item_newly_available(self, item: GetFavoritesBasketItemResponse) -> bool:
+        if item.items_available == 0:
+            return False
+
+        is_item_in_previous_scan = item.item_id in self.previous_favorites_scan_result
+        if is_item_in_previous_scan:
+            # Item was in previous scan. If it wasn't available in previous run, but is available now, return true
+            return self.previous_favorites_scan_result[item.item_id] == 0
+        else:
+            # Item has availability, but was not in previous scan (new favorite item or first run)
+            return True
+
+    def __notify_new_item_available(self, item: GetFavoritesBasketItemResponse) -> None:
+        logging.info(f'New favorite item available in Good To Go - Store={item.display_name} Items Available={item.items_available}')
