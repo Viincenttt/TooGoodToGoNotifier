7810e0355c483f524cb2d6a239c3909a5ff13b87
vincent kok <vincentkok@live.nl>
2022-10-02 11:46:14 +0200

    Log output to file

--- src/__main__.py
+++ src/__main__.py
@@ -3,13 +3,20 @@ from authentication.authenticator import Authenticator
 
 from config import AppConfiguration
 import logging
+from logging.handlers import RotatingFileHandler
 
 from scan.favoritesscanner import FavoritesScanner
 
 def main():
     app_config = AppConfiguration()
     logging_level = getattr(logging, app_config.logging_level.upper(), None)
-    logging.basicConfig(level=logging_level)
+    logging.basicConfig(
+        level=logging_level,
+        handlers=[
+            RotatingFileHandler('debug.log', maxBytes=100000, backupCount=10),
+            logging.StreamHandler()
+        ]
+    )
     
     scanner = FavoritesScanner(app_config.email)
     scanner.scan_continuously()
