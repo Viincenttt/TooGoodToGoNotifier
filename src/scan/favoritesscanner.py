75ca925a109b174963f2706cab6588f6a9a99953
vincent kok <vincentkok@live.nl>
2022-10-02 13:33:52 +0200

    Only scan between 7 and 22

--- src/scan/favoritesscanner.py
+++ src/scan/favoritesscanner.py
@@ -3,6 +3,7 @@ import logging
 import random
 import time
 from api.models import GetFavoritesBasketItemResponse
+import datetime
 
 from authentication.authenticator import Authenticator
 
@@ -18,11 +19,17 @@ class FavoritesScanner:
 
     def scan_continuously(self) -> None:
         while (True):
-            self.scan_favorites()
+            now = datetime.datetime.now()
+            if (now.hour >= 7 and now.hour <= 22):
+                logging.info('Start scanning favorites')
+                self.scan_favorites()
+            else:
+                logging.info('Not scanning because we dont expect any new items during these times')
 
             time_to_sleep = random.randint(self.time_between_scanning_in_seconds_from, self.time_between_scanning_in_seconds_to)
             logging.info(f"Scanning complete, sleeping for {time_to_sleep} seconds")
             time.sleep(time_to_sleep)
+            
 
     def scan_favorites(self) -> None:
         try:
